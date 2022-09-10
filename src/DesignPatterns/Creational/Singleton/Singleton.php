6e49e36feadf6d1c3bbe22ef656d277c79d4c6d7
pszewczykpl <pszewczykpl@gmail.com>
2022-09-10 13:07:17 +0200

    Added Singleton Design Pattern example

---
+++ src/DesignPatterns/Creational/Singleton/Singleton.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SimpleExamples\DesignPatterns\Creational\Singleton;
+
+use Exception;
+
+final class Singleton
+{
+    private static ?Singleton $instance = null;
+
+    public static function getInstance(): Singleton
+    {
+        if (self::$instance === null) self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function __construct() {}
+
+    private function __clone() {}
+
+    public function __wakeup() {
+        throw new Exception("Cannot unserialize Singleton");
+    }
+}
